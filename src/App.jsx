735b326d858b4c5f7486f77b7436017ced5efafc
jschmitt <schmitty890@gmail.com>
2019-06-02 14:17:42 -0400

    dynamic composition

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,41 @@ class IssueFilter extends React.Component {
   }
 }
 
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : ''}</td>
+        <td>{issue.title}</td>
+      </tr>
+    )
+  }
+}
+
 class IssueTable extends React.Component {
   render() {
+    const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
     return (
-      <div>This is a placeholder for a table of Issues.</div>
+      <table className="bordered-table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Status</th>
+            <th>Owner</th>
+            <th>Created</th>
+            <th>Effort</th>
+            <th>Completion Date</th>
+            <th>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
     )
   }
 }
@@ -24,6 +55,19 @@ class IssueAdd extends React.Component {
   }
 }
 
+const issues = [
+  {
+    id: 1, status: 'Open', owner: 'Ravan',
+    created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+    title: 'Error in console when clicking Add',
+  },
+  {
+    id: 2, status: 'Assigned', owner: 'Eddie',
+    created: new Date('2016-08-16'), effort: 14, completionDate: new Date('2016-08-30'),
+    title: 'Missing bottom border on panel',
+  },
+];
+
 class IssueList extends React.Component {
   render() {
     return (
@@ -31,7 +75,7 @@ class IssueList extends React.Component {
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable />
+        <IssueTable issues={issues} />
         <hr />
         <IssueAdd />
       </div>
